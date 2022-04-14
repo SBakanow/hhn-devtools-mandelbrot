e4ffd9a97b7759e11e23387eb2339f8242e1ff05
Sergej Bakanow <s.bakanow@gmail.com>
2022-04-14 11:41:26 +0200

    Add RenderServiceUsageDemo class

--- apis/src/test/java/de/hhn/it/devtools/apis/examples/renderservice/RenderServiceUsageDemo.java
+++ apis/src/test/java/de/hhn/it/devtools/apis/examples/renderservice/RenderServiceUsageDemo.java
@@ -1,2 +1,4 @@
-package de.hhn.it.devtools.apis.examples.renderservice;public class RenderServiceUsageDemo {
+package de.hhn.it.devtools.apis.examples.renderservice;
+
+public class RenderServiceUsageDemo {
 }
