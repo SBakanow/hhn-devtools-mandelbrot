1d039fe8258794b3863d90bf717d771ad93333ce
Marvin <simon.marvin.1@web.de>
2022-04-16 17:11:56 +0200

    feat: Update the RenderListener with the necessary interface to receive the calculation from the RenderService.

--- apis/src/main/java/de/hhn/it/devtools/apis/renderer/RenderListener.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/renderer/RenderListener.java
@@ -1,5 +1,16 @@
 package de.hhn.it.devtools.apis.renderer;
 
+/**
+ * Listener interface which the JavaFxOpenGL renderer uses to get the updated byte stream.
+ * The byte stream provided by this interface will be used in the vertex buffer.
+ */
 public interface RenderListener {
 
+  /**
+   * The method that gets called after any change in the render service.
+   * This method will be called after every new calculation.
+   *
+   * @param buffer The buffer which will be used for the vertex buffer
+   */
+  void updateVertexBuffer(byte[] buffer);
 }
