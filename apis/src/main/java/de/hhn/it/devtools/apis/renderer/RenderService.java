f1a63ab1cd2a55eae3c7b3d986a42744bfceb26b
Timo Kugler <tikugler@stud.hs-heilbronn.de>
2022-04-16 14:06:05 +0200

    Edited and added JavaDoc

--- apis/src/main/java/de/hhn/it/devtools/apis/renderer/RenderService.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/renderer/RenderService.java
@@ -4,21 +4,23 @@ import de.hhn.it.devtools.apis.exceptions.IllegalParameterException;
 
 /**
  * This RenderService is an interface for calculating the currently selected shape.
- * The representation is done in the JavaFxOpenGL Renderer with the data the interface provides.
+ * The representation is done in the JavaFxOpenGL Renderer in 3D with the data the
+ * interface provides.
  */
 public interface RenderService {
 
   /**
    * In this function the currently selected shape gets calculated and preprocessed,
-   * so it can be added into a Vertex.
+   * so it can be added into a Vertex. The shape is important to define the layout of
+   * the data (points of the selected shape) that's stored in an array.
    *
    * @param shape shape to be rendered
-   * @throws IllegalParameterException if the shape does not exist
+   * @throws IllegalParameterException if the shape is invalid
    */
   void render(Shape shape) throws IllegalParameterException;
 
   /**
-   * Updates the zoom value, which the render function uses.
+   * Calculates and updates the new zoom value, which are used by the render function.
    *
    * @param zoomX specifies scale factors along the X axis
    * @param zoomY specifies scale factors along the Y axis
@@ -27,7 +29,7 @@ public interface RenderService {
   void zoom(double zoomX, double zoomY, double zoomZ);
 
   /**
-   * Updates the rotation values, which the render function uses.
+   * Calculates and updates the new rotation values, which are used by the render function.
    *
    * @param angle     specifies the angle of the rotation in degrees
    * @param rotationX currentRotation on the X axis
@@ -37,13 +39,13 @@ public interface RenderService {
   void rotate(double angle, double rotationX, double rotationY, double rotationZ);
 
   /**
-   * Updates the color values, which the render function uses.
+   * Calculates and updates the new color values, which are used by the render function.
    *
    * @param red   represents the red channel of the color spectre in OpenGL
    * @param green represents the green channel of the color spectre in OpenGL
    * @param blue  represents the blue channel of the color spectre in OpenGL
    * @param alpha represents the alpha channel of the color spectre in OpenGL
-   * @throws IllegalParameterException if the values are out of range of the OpenGLColor standard [0,1]
+   * @throws IllegalParameterException if values are out of range of the OpenGLColor standard [0,1]
    */
   void changeColor(float red, float green, float blue, float alpha)
       throws IllegalParameterException;
@@ -57,7 +59,7 @@ public interface RenderService {
   void addCallback(RenderListener renderListener) throws IllegalParameterException;
 
   /**
-   * Removes a listener.
+   * Removes a given listener.
    *
    * @param renderListener listener to be removed
    * @throws IllegalParameterException if the listener is a null reference
--- apis/src/main/java/de/hhn/it/devtools/apis/renderer/Shape.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/renderer/Shape.java
@@ -1,8 +1,17 @@
 package de.hhn.it.devtools.apis.renderer;
 
+/**
+ * Available shapes/pattern of the renderer.
+ */
 public enum Shape {
+  // The Mandelbrot function is a recursive function: z^2 + c . Iteration start: z=0 .
   MANDELBROT,
+
   CUBE,
+
+  // Random generated function (or parameters???) results in random fractals.
   RANDOM_FRACTAL,
+
+  // Julia set is a recursion of a function.
   JULIA
 }
