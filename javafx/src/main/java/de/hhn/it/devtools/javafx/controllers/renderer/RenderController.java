8c2c5e75ea8b6c42c9cf582174207220f808c09f
Sergej Bakanow <sbakanow@stud.hs-heilbronn.de>
2022-04-26 11:40:29 +0200

    Move shader compilation + linking into seperate class and add camera implementation

--- javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/renderer/RenderController.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/renderer/RenderController.java
@@ -1,8 +1,6 @@
 package de.hhn.it.devtools.javafx.controllers.renderer;
 
-import static org.lwjgl.opengl.GL11.GL_FALSE;
 import static org.lwjgl.opengl.GL11.GL_FLOAT;
-import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
 import static org.lwjgl.opengl.GL11.GL_PROJECTION;
 import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
 import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
@@ -10,31 +8,14 @@ import static org.lwjgl.opengl.GL11.glDrawElements;
 import static org.lwjgl.opengl.GL11.glLoadIdentity;
 import static org.lwjgl.opengl.GL11.glMatrixMode;
 import static org.lwjgl.opengl.GL11.glOrtho;
-import static org.lwjgl.opengl.GL11.glRotatef;
 import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
 import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
 import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
 import static org.lwjgl.opengl.GL15.glBindBuffer;
 import static org.lwjgl.opengl.GL15.glBufferData;
 import static org.lwjgl.opengl.GL15.glGenBuffers;
-import static org.lwjgl.opengl.GL20.GL_COMPILE_STATUS;
-import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
-import static org.lwjgl.opengl.GL20.GL_INFO_LOG_LENGTH;
-import static org.lwjgl.opengl.GL20.GL_LINK_STATUS;
-import static org.lwjgl.opengl.GL20.GL_VERTEX_SHADER;
-import static org.lwjgl.opengl.GL20.glAttachShader;
-import static org.lwjgl.opengl.GL20.glCompileShader;
-import static org.lwjgl.opengl.GL20.glCreateProgram;
-import static org.lwjgl.opengl.GL20.glCreateShader;
 import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
 import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
-import static org.lwjgl.opengl.GL20.glGetProgramInfoLog;
-import static org.lwjgl.opengl.GL20.glGetProgrami;
-import static org.lwjgl.opengl.GL20.glGetShaderInfoLog;
-import static org.lwjgl.opengl.GL20.glGetShaderi;
-import static org.lwjgl.opengl.GL20.glLinkProgram;
-import static org.lwjgl.opengl.GL20.glShaderSource;
-import static org.lwjgl.opengl.GL20.glUseProgram;
 import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
 import static org.lwjgl.opengl.GL30.glBindVertexArray;
 import static org.lwjgl.opengl.GL30.glGenVertexArrays;
@@ -51,7 +32,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
 
 public class RenderController extends Controller implements Initializable {
 
@@ -64,43 +47,12 @@ public class RenderController extends Controller implements Initializable {
 
   Pane stage;
 
-  private float counter = 0;
-  private float red = 0;
-  private float green = 0;
-  private float blue = 0;
-
-  // Test -----------------------------------------------------------------------------------------
-  private String vertexShaderSrc = "#version 330 core\n" +
-      "layout (location=0) in vec3 aPos;\n" +
-      "layout (location=1) in vec4 aColor;\n" +
-      "\n" +
-      "out vec4 fColor;\n" +
-      "\n" +
-      "void main()\n" +
-      "{\n" +
-      "    fColor = aColor;\n" +
-      "    gl_Position = vec4(aPos, 1.0);\n" +
-      "}";
-
-  private String fragmentShaderSrc = "#version 330 core\n" +
-      "\n" +
-      "in vec4 fColor;\n" +
-      "\n" +
-      "out vec4 color;\n" +
-      "\n" +
-      "void main()\n" +
-      "{\n" +
-      "    color = fColor;\n" +
-      "}";
-
-  private int vertexID, fragmentID, shaderProgram;
-
   private float[] vertexArray = {
       // position            //color
-       0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f, 1.0f,   // Bottom right 0
-      -0.5f,  0.5f, 0.0f,    0.0f, 1.0f, 0.0f, 1.0f,   // Top left     1
-       0.5f,  0.5f, 0.0f,    0.0f, 0.0f, 1.0f, 1.0f,   // Top right    2
-      -0.5f, -0.5f, 0.0f,    1.0f, 1.0f, 0.0f, 1.0f    // Bottom left  3
+      100.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,   // Bottom right 0
+      0.5f, 100.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,   // Top left     1
+      100.5f, 100.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // Top right    2
+      0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f    // Bottom left  3
   };
 
   // IMPORTANT: Must be in counter-clockwise order
@@ -115,7 +67,8 @@ public class RenderController extends Controller implements Initializable {
   };
 
   private int vaoID, vboID, eboID;
-  // Test -----------------------------------------------------------------------------------------
+  private Shader defaultShader;
+  public Camera camera;
 
   public RenderController() {
     logger.debug("Template Controller created. Hey, if you have copied me, update this message!");
@@ -133,41 +86,14 @@ public class RenderController extends Controller implements Initializable {
       glOrtho(0.0, canvas.getScene().getWidth(), 0.0, canvas.getScene().getHeight(), -1.0, 100.0);
     });
     canvas.onInitialize(() -> {
-      // Test ----------------------------------------------------------------------------------------
-      vertexID = glCreateShader(GL_VERTEX_SHADER);
-      glShaderSource(vertexID, vertexShaderSrc);
-      glCompileShader(vertexID);
+      this.camera = new Camera(new Vector2f());
+      defaultShader =
+          new Shader(
+              System.getProperty("user.dir")
+                  + "/src/main/java/de/hhn/it/devtools/javafx/controllers/renderer/shader/default.glsl");
+      defaultShader.compile();
 
-      int success = glGetShaderi(vertexID, GL_COMPILE_STATUS);
-      if (success == GL_FALSE) {
-        int len = glGetShaderi(vertexID, GL_INFO_LOG_LENGTH);
-        System.out.println("ERROR: 'defaultShader.glsl'\n\tVertex shader compilation failed.");
-        System.out.println(glGetShaderInfoLog(vertexID, len));
-      }
-
-      fragmentID = glCreateShader(GL_FRAGMENT_SHADER);
-      glShaderSource(fragmentID, fragmentShaderSrc);
-      glCompileShader(fragmentID);
-
-      success = glGetShaderi(fragmentID, GL_COMPILE_STATUS);
-      if (success == GL_FALSE) {
-        int len = glGetShaderi(fragmentID, GL_INFO_LOG_LENGTH);
-        System.out.println("ERROR: 'defaultShader.glsl'\n\tfragment shader compilation failed.");
-        System.out.println(glGetShaderInfoLog(fragmentID, len));
-      }
-
-      shaderProgram = glCreateProgram();
-      glAttachShader(shaderProgram, vertexID);
-      glAttachShader(shaderProgram, fragmentID);
-      glLinkProgram(shaderProgram);
-
-      success = glGetProgrami(shaderProgram, GL_LINK_STATUS);
-      if (success == GL_FALSE) {
-        int len = glGetProgrami(shaderProgram, GL_INFO_LOG_LENGTH);
-        System.out.println("ERROR: 'defaultShader.glsl'\n\tlinking of shaders failed.");
-        System.out.println(glGetProgramInfoLog(shaderProgram, len));
-      }
-
+      // Generate VAO, VBO and EBO buffer objects and send to GPU
       vaoID = glGenVertexArrays();
       glBindVertexArray(vaoID);
 
@@ -196,14 +122,17 @@ public class RenderController extends Controller implements Initializable {
       glVertexAttribPointer(0, positionsSize, GL_FLOAT, false, vertexSizeInBytes, 0);
       glEnableVertexAttribArray(0);
 
-      glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeInBytes, positionsSize * floatSizeInBytes);
+      glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeInBytes,
+          positionsSize * floatSizeInBytes);
       glEnableVertexAttribArray(1);
-      // Test ----------------------------------------------------------------------------------------
+
     });
     canvas.onRender(() -> {
-      // Bind shader program
-      glUseProgram(shaderProgram);
-
+      GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+      camera.position.x -= 0.005 * 50.0f;
+      defaultShader.use();
+      defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
+      defaultShader.uploadMat4f("uView", camera.getViewMatrix());
       // Bind the VAO that we're using
       glBindVertexArray(vaoID);
 
@@ -213,15 +142,13 @@ public class RenderController extends Controller implements Initializable {
 
       glDrawElements(GL_TRIANGLES, elementArray.length, GL_UNSIGNED_INT, 0);
 
-      glRotatef(1.0f, 1.0f, 0.0f, 0.0f);
-
       // Unbind everything
       glDisableVertexAttribArray(0);
       glDisableVertexAttribArray(1);
 
       glBindVertexArray(0);
 
-      glUseProgram(0);
+      defaultShader.detach();
     });
     return canvas;
   }
