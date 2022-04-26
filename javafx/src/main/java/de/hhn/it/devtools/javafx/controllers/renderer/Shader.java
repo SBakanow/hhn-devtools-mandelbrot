fa16e09dd089b8aa000c26b81c3b2c6f313f8af7
Sergej Bakanow <sbakanow@stud.hs-heilbronn.de>
2022-04-26 13:27:45 +0200

    Add different uploadmethods

--- javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/renderer/Shader.java
+++ javafx/src/main/java/de/hhn/it/devtools/javafx/controllers/renderer/Shader.java
@@ -17,6 +17,12 @@ import static org.lwjgl.opengl.GL20.glGetShaderi;
 import static org.lwjgl.opengl.GL20.glGetUniformLocation;
 import static org.lwjgl.opengl.GL20.glLinkProgram;
 import static org.lwjgl.opengl.GL20.glShaderSource;
+import static org.lwjgl.opengl.GL20.glUniform1f;
+import static org.lwjgl.opengl.GL20.glUniform1i;
+import static org.lwjgl.opengl.GL20.glUniform2f;
+import static org.lwjgl.opengl.GL20.glUniform3f;
+import static org.lwjgl.opengl.GL20.glUniform4f;
+import static org.lwjgl.opengl.GL20.glUniformMatrix3fv;
 import static org.lwjgl.opengl.GL20.glUniformMatrix4fv;
 import static org.lwjgl.opengl.GL20.glUseProgram;
 
@@ -24,12 +30,17 @@ import java.io.IOException;
 import java.nio.FloatBuffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import org.joml.Matrix3f;
 import org.joml.Matrix4f;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
 import org.lwjgl.BufferUtils;
 
 public class Shader {
 
   private int shaderProgramID;
+  private boolean beingUsed = false;
 
   private String vertexSource;
   private String fragmentSource;
@@ -119,19 +130,63 @@ public class Shader {
   }
 
   public void use() {
-    // Bind shader program
-    glUseProgram(shaderProgramID);
-
+    if (!beingUsed) {
+      // Bind shader program
+      glUseProgram(shaderProgramID);
+      beingUsed = true;
+    }
   }
 
   public void detach() {
     glUseProgram(0);
+    beingUsed = false;
   }
 
   public void uploadMat4f(String varName, Matrix4f mat4) {
     int varLocation = glGetUniformLocation(shaderProgramID, varName);
+    use();
     FloatBuffer matBuffer = BufferUtils.createFloatBuffer(16);
     mat4.get(matBuffer);
     glUniformMatrix4fv(varLocation, false, matBuffer);
   }
+
+  public void uploadMat3f(String varName, Matrix3f mat3) {
+    int varLocation = glGetUniformLocation(shaderProgramID, varName);
+    use();
+    FloatBuffer matBuffer = BufferUtils.createFloatBuffer(9);
+    mat3.get(matBuffer);
+    glUniformMatrix3fv(varLocation, false, matBuffer);
+  }
+
+  public void uploadVec4f(String varName, Vector4f vec) {
+    int varLocation = glGetUniformLocation(shaderProgramID, varName);
+    use();
+    glUniform4f(varLocation, vec.x, vec.y, vec.z, vec.w);
+  }
+
+  public void uploadVec3f(String varName, Vector3f vec) {
+    int varLocation = glGetUniformLocation(shaderProgramID, varName);
+    use();
+    glUniform3f(varLocation, vec.x, vec.y, vec.z);
+  }
+
+  public void uploadVec2f(String varName, Vector2f vec) {
+    int varLocation = glGetUniformLocation(shaderProgramID, varName);
+    use();
+    glUniform2f(varLocation, vec.x, vec.y);
+  }
+
+  public void uploadFloat(String varName, float val) {
+    int varLocation = glGetUniformLocation(shaderProgramID, varName);
+    use();
+    glUniform1f(varLocation, val);
+  }
+
+  public void uploadInt(String varName, int val) {
+    int varLocation = glGetUniformLocation(shaderProgramID, varName);
+    use();
+    glUniform1i(varLocation, val);
+  }
+
+
 }
